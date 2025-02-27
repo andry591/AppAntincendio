2ac3a9d0a6634530335709ac26f095b3971fd621
andrea <st10712@iismarconipieralisi.it>
2025-02-27 23:47:13 +0100

    fixed

--- app/src/main/java/com/example/appantincendio/SecondActivity.java
+++ app/src/main/java/com/example/appantincendio/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.appantincendio;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
-    //metodo OnCreate che fa visualizzare la pagina
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Get the button from the layout
+        final Button buttonInvia = findViewById(R.id.indietro);
+
+        if (buttonInvia != null) {
+            // Assign a listener to the button
+            buttonInvia.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+            });
+        }
     }
 }

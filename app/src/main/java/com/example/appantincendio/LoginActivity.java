716a889a12788a652a6946e05b7bd83e8e30b3e4
st10712 <st10712@iismarconipieralisi.it>
2025-03-07 11:10:43 +0100

    fixed app

--- app/src/main/java/com/example/appantincendio/LoginActivity.java
+++ app/src/main/java/com/example/appantincendio/LoginActivity.java
@@ -2,8 +2,10 @@ package com.example.appantincendio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SecondActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Get the button from the layout
         final Button buttonInvia = findViewById(R.id.indietro);
@@ -26,11 +34,13 @@ public class SecondActivity extends AppCompatActivity {
             buttonInvia.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
 
             });
         }
+
+
     }
 }
--- app/src/main/java/com/example/appantincendio/MainActivity.java
+++ app/src/main/java/com/example/appantincendio/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.appantincendio;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +15,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the button from the layout
-        final Button buttonInvia = findViewById(R.id.buttonInvia);
+        final Button registratiButton = findViewById(R.id.registrati);
+        final Button loginButton = findViewById(R.id.login);
 
-        if (buttonInvia != null) {
+        if (registratiButton != null) {
             // Assign a listener to the button
-            buttonInvia.setOnClickListener(new View.OnClickListener() {
+            registratiButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                    startActivity(intent);
+                    startActivity(new Intent(MainActivity.this, RegisterActivity.class));
                 }
+            });
+        }
 
+        if (loginButton != null) {
+            // Assign a listener to the button
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
             });
         }
     }
+
 }

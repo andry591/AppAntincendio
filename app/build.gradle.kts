5c45da9595f72b1d446b8e9dfa406f520731e431
st10712 <st10712@iismarconipieralisi.it>
2025-03-07 11:25:08 +0100

    added Gson library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.activity)
+    //aggiunta di Gson
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
